b736034e67771f8741bb95283bc1d35ffd63f9a2
billtomking <38128272+billtomking@users.noreply.github.com>
2018-12-21 10:40:20 +0800

    Update 加密.py

--- 文本加密解密/加密.py
+++ 文本加密解密/加密.py
@@ -1,6 +1,9 @@
+#之后应该可以将字数转换部分集合到一个函数里
 ming_wen = input('请输入明文（带空格）')
 ming_wen = list(ming_wen.split())
 mingl = len(ming_wen)
+mi_wen = []
+n = 0
 def chuang_yao(n):#用于产生随机密钥，之后可以考虑修改让用户自行输入
     import random
     if n == None:
@@ -84,6 +87,161 @@ def chuang_yao(n):#用于产生随机密钥，之后可以考虑修改让用户�
             b = 'Z'
         mi_yao.append(str(b))
     return mi_yao
+def zitoshu(yuan):#用于将文字转换成数字，以便后续计算
+    yuan = str(yuan)
+    if yuan == '0':
+        return 0
+    elif yuan == '1':
+        return 1
+    elif yuan == '2':
+        return 2
+    elif yuan == '3':
+        return 3
+    elif yuan == '4':
+        return 4
+    elif yuan == '5':
+        return 5
+    elif yuan == '6':
+        return 6
+    elif yuan == '7':
+        return 7
+    elif yuan == '8':
+        return 8
+    elif yuan == '9':
+        return 9
+    elif yuan == 'A':
+        return 10
+    elif yuan == 'B':
+        return 11
+    elif yuan == 'C':
+        return 12
+    elif yuan == 'D':
+        return 13
+    elif yuan == 'E':
+        return 14
+    elif yuan == 'F':
+        return 15
+    elif yuan == 'G':
+        return 16
+    elif yuan == 'H':
+        return 17
+    elif yuan == 'I':
+        return 18
+    elif yuan == 'J':
+        return 19
+    elif yuan == 'K':
+        return 20
+    elif yuan == 'L':
+        return 21
+    elif yuan == 'M':
+        return 22
+    elif yuan == 'N':
+        return 23
+    elif yuan == 'O':
+        return 24
+    elif yuan == 'P':
+        return 25
+    elif yuan == 'Q':
+        return 26
+    elif yuan == 'R':
+        return 27
+    elif yuan == 'S':
+        return 28
+    elif yuan == 'T':
+        return 29
+    elif yuan == 'U':
+        return 30
+    elif yuan == 'V':
+        return 31
+    elif yuan == 'W':
+        return 32
+    elif yuan == 'X':
+        return 33
+    elif yuan == 'Y':
+        return 34
+    elif yuan == 'Z':
+        return 35
+def shutozi(yuan):#用于将计算结果转换成文字
+    yuan = str(yuan)
+    if yuan == '0':
+        return '0'
+    elif yuan == '1':
+        return '1'
+    elif yuan == '2':
+        return '2'
+    elif yuan == '3':
+        return '3'
+    elif yuan == '4':
+        return '4'
+    elif yuan == '5':
+        return '5'
+    elif yuan == '6':
+        return '6'
+    elif yuan == '7':
+        return '7'
+    elif yuan == '8':
+        return '8'
+    elif yuan == '9':
+        return '9'
+    elif yuan == '10':
+        return 'A'
+    elif yuan == '11':
+        return 'B'
+    elif yuan == '12':
+        return 'C'
+    elif yuan == '13':
+        return 'D'
+    elif yuan == '14':
+        return 'E'
+    elif yuan == '15':
+        return 'F'
+    elif yuan == '16':
+        return 'G'
+    elif yuan == '17':
+        return 'H'
+    elif yuan == '18':
+        return 'I'
+    elif yuan == '19':
+        return 'J'
+    elif yuan == '20':
+        return 'K'
+    elif yuan == '21':
+        return 'L'
+    elif yuan == '22':
+        return 'M'
+    elif yuan == '23':
+        return 'N'
+    elif yuan == '24':
+        return 'O'
+    elif yuan == '25':
+        return 'P'
+    elif yuan == '26':
+        return 'Q'
+    elif yuan == '27':
+        return 'R'
+    elif yuan == '28':
+        return 'S'
+    elif yuan == '29':
+        return 'T'
+    elif yuan == '30':
+        return 'U'
+    elif yuan == '31':
+        return 'V'
+    elif yuan == '32':
+        return 'W'
+    elif yuan == '33':
+        return 'X'
+    elif yuan == '34':
+        return 'Y'
+    elif yuan == '35':
+        return 'Z'
 mi_yao = chuang_yao(12)
+while n < len(mi_yao):#将密钥转换成数字
+    mi_yao[n] = zitoshu(mi_yao[n])
+    n += 1
+n = 0
+while n < len(ming_wen):#将明文转换成数字
+    ming_wen[n] = zitoshu(ming_wen[n])
+    n += 1
+
 print(mi_yao)
-    
